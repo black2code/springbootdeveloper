f30fcdd6b3438cf08793708d3dd325b63fa0c05e
black2code <alsdnr1080@gmail.com>
2023-06-22 17:40:31 +0900

    Add: test 생성

---
+++ src/test/java/com/minwook/springbootdeveloper/SpringbootdeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minwook.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
