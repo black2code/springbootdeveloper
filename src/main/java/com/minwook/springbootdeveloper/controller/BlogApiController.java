aa954981f00b0c8d8e612e5b3509d5cadc8f9459
black2code <alsdnr1080@gmail.com>
2023-06-22 17:38:34 +0900

    Add: controller 생성

---
+++ src/main/java/com/minwook/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,25 @@
+package com.minwook.springbootdeveloper.controller;
+
+import com.minwook.springbootdeveloper.domain.Article;
+import com.minwook.springbootdeveloper.dto.AddArticleRequest;
+import com.minwook.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTPResponse Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
