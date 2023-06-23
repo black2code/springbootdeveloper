98540c8ec14e6607c1c26f539c82f7e5121aacfc
black2code <alsdnr1080@gmail.com>
2023-06-23 16:11:41 +0900

    Add: Servicce 생성

--- src/main/java/com/minwook/springbootdeveloper/service/BlogService.java
+++ src/main/java/com/minwook/springbootdeveloper/service/BlogService.java
@@ -2,9 +2,14 @@ package com.minwook.springbootdeveloper.service;
 
 import com.minwook.springbootdeveloper.domain.Article;
 import com.minwook.springbootdeveloper.dto.AddArticleRequest;
+import com.minwook.springbootdeveloper.dto.UpdateArticleRequest;
 import com.minwook.springbootdeveloper.repository.BlogRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor  // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service
@@ -14,5 +19,28 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
