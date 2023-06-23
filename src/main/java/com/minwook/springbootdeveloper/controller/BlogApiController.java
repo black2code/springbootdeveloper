f3998d8c9893d11cbf94be25f767f1dbb8a7109a
black2code <alsdnr1080@gmail.com>
2023-06-23 16:11:51 +0900

    Add: Controller 생성

--- src/main/java/com/minwook/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/minwook/springbootdeveloper/controller/BlogApiController.java
@@ -2,13 +2,16 @@ package com.minwook.springbootdeveloper.controller;
 
 import com.minwook.springbootdeveloper.domain.Article;
 import com.minwook.springbootdeveloper.dto.AddArticleRequest;
+import com.minwook.springbootdeveloper.dto.ArticleResponse;
+import com.minwook.springbootdeveloper.dto.UpdateArticleRequest;
 import com.minwook.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController // HTTPResponse Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -21,5 +24,41 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticle() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
