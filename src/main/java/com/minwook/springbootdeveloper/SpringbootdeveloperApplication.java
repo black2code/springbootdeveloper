0bbac196e80ec5d03fadd199a34a410471ae4f35
black2code <alsdnr1080@gmail.com>
2023-06-26 17:24:12 +0900

    Add: datetime 추가

--- src/main/java/com/minwook/springbootdeveloper/SpringbootdeveloperApplication.java
+++ src/main/java/com/minwook/springbootdeveloper/SpringbootdeveloperApplication.java
@@ -2,7 +2,9 @@ package com.minwook.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootdeveloperApplication {
 
