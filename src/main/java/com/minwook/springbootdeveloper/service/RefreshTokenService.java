4ec797cf1850a5eca6271e91144d69ff1d4247da
black2code <alsdnr1080@gmail.com>
2023-06-28 01:49:56 +0900

    Add : JWT 구현

---
+++ src/main/java/com/minwook/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.minwook.springbootdeveloper.service;
+
+
+import com.minwook.springbootdeveloper.domain.RefreshToken;
+import com.minwook.springbootdeveloper.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
